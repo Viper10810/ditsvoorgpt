f963dcaaf6bb8cc2fef484083bbcef4b182936df
Viper10810 <122435068+Viper10810@users.noreply.github.com>
2023-04-19 12:56:47 +0200

    Add files via upload

--- school.php
+++ school.php
@@ -47,6 +47,7 @@ $users = $obj->getSchool();
                <tr>
                    <td><?php echo $user['schoolID'];?></td>
                    <td><?php echo $user['naam'];?></td>
+
                    <td class="Edit">
                        <a class="btn btn-primary mr-2 btn-sm" href="./school/editSchool.php?id=<?php echo $user['schoolID']; ?>">Aanpassen</a>
                    </td>      
@@ -54,6 +55,52 @@ $users = $obj->getSchool();
                        <a class="btn btn-danger mr-2 btn-sm" href="./school/deleteSchool.php?id=<?php echo $user['schoolID']; ?>">Verwijderen</a>
                    </td> 
                </tr>
+               <?php
+
+
+// Connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "toernooi";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$xml = simplexml_load_file("scholen.xml");
+foreach ($xml->children() as $school) {
+  $id = $school->ID;
+  $voornaam = $school->Voornaam;
+  $achternaam = $school->Achternaam;
+  $schoolID = $school->SchoolID;
+  echo "ID: $id<br>";
+  echo "Voornaam: $voornaam<br>";
+  echo "Achternaam: $achternaam<br>";
+  echo "SchoolID: $schoolID<br><br>";
+}
+
+// Retrieve data from the scholen table
+$sql = "SELECT * FROM scholen";
+$result = mysqli_query($conn, $sql);
+
+// Display the data on the index page
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+       echo "ID: " . (isset($speler["ID"]) ? $speler["ID"] : "") . "<br>";
+       echo "Scholen: " . (isset($speler["Scholen"]) ? $speler["Scholen"] : "") . "<br>";
+        echo "<br>";
+    }
+} else {
+    echo "Geen scholen gevonden.";
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
 
                <?php endforeach; ?>               
                     <td class="index">
